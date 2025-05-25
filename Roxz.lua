e85d4a28e0c44588a6814a17d7977f896403246a
Newroxy <Rolosxy@gmail.com>
2025-05-25 14:13:38 -0300

    Update Roxz.lua

--- Roxz.lua
+++ Roxz.lua
@@ -1,5 +1,5 @@
 local placeId = game.PlaceId
-local universeId = game.GameId or UniverseID -- Usa `game.GameId` se `UniverseID` não estiver definido
+local universeId = game.GameId or UniverseID
 
 if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/RoxzHub.lua"))()
