d340523770b520e6d843f5304febc0f7353f5cb2
Newroxy <Rolosxy@gmail.com>
2025-05-25 18:36:27 -0300

    Update RoxzPremiun.lua

--- RoxzPremiun.lua
+++ RoxzPremiun.lua
@@ -2,7 +2,7 @@ local placeId = game.PlaceId
 local universeId = game.GameId or UniverseID
 
 if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/RoxzHub.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/RoxzHubPremiun.lua"))()
 elseif universeId == 7436755782 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/GrowAGraden.lua"))()
 end
