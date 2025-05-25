30e98ee6d143fdfc79961f8672fe18c3938f11aa
Newroxy <Rolosxy@gmail.com>
2025-05-25 14:13:06 -0300

    Uptade Roxz.lua

---
+++ Roxz.lua
@@ -0,0 +1,8 @@
+local placeId = game.PlaceId
+local universeId = game.GameId or UniverseID -- Usa `game.GameId` se `UniverseID` não estiver definido
+
+if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/RoxzHub.lua"))()
+elseif universeId == 7436755782 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/GrowAGraden.lua"))()
+end
