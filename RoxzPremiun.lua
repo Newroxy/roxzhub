711fcdbc636ded48f013c3eaf642cb41e484e13e
Roxz <Rolosxy@gmail.com>
2025-05-31 13:40:05 -0300

    Update RoxzPremiun.lua

--- RoxzPremiun.lua
+++ RoxzPremiun.lua
@@ -6,3 +6,4 @@ if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
 elseif universeId == 7436755782 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/GrowAGraden.lua"))()
 end
+e
