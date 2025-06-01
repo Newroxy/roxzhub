e626535ea2741cb7841d939bacc4258127225887
Roxz <Rolosxy@gmail.com>
2025-06-01 20:25:23 -0300

    Update RoxzPremiun.lua

--- RoxzPremiun.lua
+++ RoxzPremiun.lua
@@ -1,8 +1,10 @@
 local placeId = game.PlaceId
-local universeId = game.GameId or UniverseID
+local universeId = game.GameId
 
 if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/RoxzHubPremiun.lua"))()
 elseif universeId == 7436755782 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/GrowAGraden.lua"))()
+elseif universeId == 7018190066 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Newroxy/Roxzhub/refs/heads/main/Roxz-Deadrails.lua"))()
 end
